68da0d562ac3010c81195592b426defab9e3282d
filosof777 <olimjonkamalov@mail.ru>
2022-01-12 14:07:39 +0500

    initial commit

---
+++ js/mac-obj.js
@@ -0,0 +1,23 @@
+let macBook = {
+  gold: [
+    'https://cdn.macbro.uz/macbro/13131aa5-c6f7-4bef-8518-e92be1db4ad8',
+    'https://cdn.macbro.uz/macbro/55c4aad5-3bbe-48b4-9074-f6b387eb37c2',
+    'https://cdn.macbro.uz/macbro/c503bb8d-8e71-44d5-81a7-3ec067ce2328',
+    'https://cdn.macbro.uz/macbro/643ce8b1-d06f-4fd5-84f2-838bc17b7b9f',
+    'https://cdn.macbro.uz/macbro/e970817f-ce10-4620-bc8b-343c26e0ad05'
+  ],
+  silver: [
+   'https://cdn.macbro.uz/macbro/271ebc90-5018-4636-a3ce-4a96ba1aca36',
+   'https://cdn.macbro.uz/macbro/d015e206-9a55-4fe3-a7f9-749889ee16e1',
+   'https://cdn.macbro.uz/macbro/cf0894d4-199a-47e8-ae41-54ac5bd6517f',
+   'https://cdn.macbro.uz/macbro/5862b288-7fd8-4589-83d0-45f9c2a81abc',
+   'https://cdn.macbro.uz/macbro/24086655-8b18-481b-8261-b6c70a0fb9d7'
+  ],
+  gray: [
+   'https://cdn.macbro.uz/macbro/97d5997d-bdb3-43c6-b417-c44b5a0c75a3',
+   'https://cdn.macbro.uz/macbro/abf793d6-19be-47bb-809d-adfc4314811c',
+   'https://cdn.macbro.uz/macbro/7873cda1-1854-4524-a919-07e34576cb08',
+   'https://cdn.macbro.uz/macbro/4931c161-2138-4976-9784-47f4a6f580e6',
+   'https://cdn.macbro.uz/macbro/c190d5ec-36f5-4046-9818-133de39f4df6'
+  ]
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,94 @@
-let elNavList = document.querySelector('.nav__list');
-let elHeaderBtn = document.querySelector('.header__btn');
+let elProductContentList = document.querySelector('.product__content-list');
+let elProductList = document.querySelector('.product__list');
+let sum = 0;
 
-elHeaderBtn.addEventListener('click', () => {
-  elNavList.classList.toggle('show-menu');
-});
+
+// elImage.forEach(item => {
+  
+// })
+
+// macBook.gold.forEach(item => {
+//   let count = 0;
+//   elProductContentList.innerHTML = `
+//     <li class="product__content-item">
+//       <img class="product__big-image" id="product__img" src="${item}" alt="img">
+//     </li>
+//     `
+// })
+let count = 0;
+macBook.gold.forEach(item => {
+  count++;
+  if (sum == 0) {
+    // elProductContentList.innerHTML = `
+    // <li class="product__content-item">
+    //   <img class="product__big-image" id="product__img" src="${item}" alt="img">
+    // </li>
+    // `
+    elProductList.innerHTML += `
+    <li class="product__item product__item--active" value='${count}'>
+      <button class="product__btn">
+        <img class="product__img" src="${item}" alt="img">
+      </button>
+    </li>
+    `
+    sum++
+  } else {
+    elProductList.innerHTML += `
+    <li class="product__item" value='${count}'>
+      <button class="product__btn">
+        <img class="product__img" src="${item}" alt="img">
+      </button>
+    </li>
+    `
+  }
+})
+
+let elItemProduct = document.querySelectorAll('.product__item');
+
+// elItemProduct.forEach(item => {
+//   item.addEventListener('click', () => {
+//     elItemProduct.forEach(items => {
+//       items.classList.remove('product__item--active')
+//     })
+//     item.classList.add('product__item--active')
+//   })
+// })
+
+// elItemProduct.forEach(item => {
+//   item.addEventListener('click', (e) => {
+//     let imgLink = e.target.querySelector('img');
+//     document.querySelector('#product__img').src = imgLink.src
+//   })
+// })
+
+
+let elList = document.querySelector('.product__content-list');
+let width = 500;
+
+for(let item of elItemProduct){
+  console.log(elItemProduct);
+  item.addEventListener('click',()=>{
+    removeClass();
+    item.classList.add('product__item--active');
+    elList.style.transform = `translate(${width - item.value * width}px)`;
+  })
+}
+
+function removeClass(){
+  for(let item of elItemProduct){
+    item.classList.remove('product__item--active')
+  }
+}
+
+function createImage(color){
+  for(let item of macBook[color]){
+    elList.innerHTML += `
+    <li class="product__content-item">
+      <img class="product__big-image" id="product__img" src="${item}" alt="img">
+    </li>
+    `
+  }
+}
+
+createImage('gold')
 
